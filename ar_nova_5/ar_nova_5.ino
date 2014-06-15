ed10064636f63b9c273a5bb2ecc6688966516fdc
Brian Hines <brian@projectweekend.net>
2014-06-15 09:48:02 -0500

    Maybe I do need these

--- ar_nova_5/ar_nova_5.ino
+++ ar_nova_5/ar_nova_5.ino
@@ -87,6 +87,7 @@ void toggleDisableButton(void)
             lightsDisabled = true;
             Serial.println("Disable lights");
         }
+        delay(250);
     }
 }
 
@@ -95,6 +96,7 @@ void manualLightsButton(void)
 {
     if(digitalRead(manualLightsPin) == LOW){
         sendLightEvent();
+        delay(250);
     }
 }
 
